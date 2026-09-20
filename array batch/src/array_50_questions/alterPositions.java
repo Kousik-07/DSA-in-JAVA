4921990bc3e0755e6410bdf17225a8821e87f565
Kousik <kousikjana0717@gmail.com>
2026-09-20 18:12:13 +0530

    added new questions alterPositions

---
+++ array batch/src/array_50_questions/alterPositions.java
@@ -0,0 +1,21 @@
+package array_50_questions;
+import java.util.Scanner;
+public class alterPositions{
+    public static void main(String arg[]){
+        System.out.println("Enter array size: ");
+        Scanner input=new Scanner(System.in);
+        int size =input.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter "+size+" element");
+        for(int i=0; i<size; i++){
+            arr[i]=input.nextInt();
+        }
+        alterArrayPosition(arr);
+    }
+
+    public static void alterArrayPosition(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            arr[i]=
+        }
+    }
+}
